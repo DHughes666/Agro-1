4008b4eed09b7dacd697372ff0bef59ddd933444
Dhughes666 <graziemolto6@gmail.com>
2023-02-18 07:53:24 +0100

    Security checks

---
+++ unoProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unoProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unoProject.settings')
+
+application = get_asgi_application()
